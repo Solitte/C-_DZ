71acf433fa896e67968a1275fa99b1d80cac09c0
SolitE <solitte@yandex.ru>
2022-08-22 02:46:45 +0500

    Home_Work_3

---
+++ S1/3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Input integer ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.WriteLine("even number");
+}
+else
+{
+    Console.WriteLine("odd number");
+}
