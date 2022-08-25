287f7e0b01d8e91362768e8a7ef5c7fc163234cf
SolitE <solitte@yandex.ru>
2022-08-26 02:28:03 +0500

    Zadacha15

---
+++ S2/3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Proverka(int number)
+{
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Weekend");
+}
+else
+{
+    Console.WriteLine("Not weekend");
+}
+}
+
+Console.Write("Input number day week ");
+int day = Convert.ToInt32(Console.ReadLine());
+Proverka(day);
