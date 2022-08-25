71ee894fbb4388826716ad297a023e51055644b8
SolitE <solitte@yandex.ru>
2022-08-26 02:11:18 +0500

    Zadacha10

---
+++ S2/1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecondDigit(int num)
+{
+    int digit = (num%100)/10;
+    return digit;
+}
+
+Console.Write("Imput integer number ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondigit = SecondDigit(number);
+Console.WriteLine($"Second digit of {number} is {secondigit}");
