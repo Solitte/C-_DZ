f25a0dbf0d5382215a0e25abf0088451926e7605
SolitE <solitte@yandex.ru>
2022-08-26 18:05:22 +0500

    For all digits

--- S2/2/Program.cs
+++ S2/2/Program.cs
@@ -1,30 +1,33 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-void ThirdDigit(int number)
+void ThirdDigit(int number, int numberdigit)
 {
     int num=number;
-    int count = 0;
+    int count = 1;    
     while (num/10 != 0)
     {
         count ++;
         num = num/10;
     }
-    if (count >= 2)
+
+    if (count > numberdigit)
     {
-        int n = count-2;
+        int n = count-numberdigit;
         while ( n > 0)
         {
             number = number/10;
             n = n-1; 
         }
         int digit = number%10;
-        Console.WriteLine($"Third digit is {digit}");
+        Console.WriteLine($"NumberDigit is {digit}");
     }
     else
     {
-        Console.WriteLine("No Third Digit");
+        Console.WriteLine("No NumberDigit");
     }
 
 }
-Console.Write("Imput integer number ");
+Console.Write($"Input integer number ");
 int Number = Convert.ToInt32(Console.ReadLine());
-ThirdDigit(Number);
+Console.Write($"Input NumberDigit ");
+int numberdigit = Convert.ToInt32(Console.ReadLine());
+ThirdDigit(Number,numberdigit);
