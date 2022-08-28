728fe4e664d29bb0c96744c0b7483680ff2a6007
SolitE <solitte@yandex.ru>
2022-08-29 01:33:08 +0500

    Cubed_numbers

--- S3/2/Program.cs
+++ S3/2/Program.cs
@@ -5,11 +5,11 @@ double S(double x1, double y1, double z1, double x2, double y2, double z2)
    double s = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
    return s; 
 }
-Console.WriteLine("Vedite koordinaty pervoy tochki");
+Console.WriteLine("Vvedite koordinaty pervoy tochki");
 double x1 = Convert.ToDouble(Console.ReadLine());
 double y1 = Convert.ToDouble(Console.ReadLine());
 double z1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Vedite koordinaty vtoroy tochki");
+Console.WriteLine("Vvedite koordinaty vtoroy tochki");
 double x2 = Convert.ToDouble(Console.ReadLine());
 double y2 = Convert.ToDouble(Console.ReadLine());
 double z2 = Convert.ToDouble(Console.ReadLine());
---
+++ S3/3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void CubedNumbers(int N)
+{
+    int count = 1;
+    while (count <= N)
+    {
+        int i = Convert.ToInt32(Math.Pow(count,3));
+        count++;
+        Console.Write($"{i} ");
+    }
+}
+Console.WriteLine("Vvedite naturalnoe chislo");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+   CubedNumbers(number);
+}
+else
+{
+    Console.WriteLine($"Vvedenoe chislo {number} ne naturalnoe");
+}
+
