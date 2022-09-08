27b573418016e87216381f767f2f9e437a93330d
SolitE <solitte@yandex.ru>
2022-09-08 23:21:36 +0500

    S6_1

---
+++ S6/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] InputArray(int size)
+{
+    int[] array = new int[size];
+    Console.WriteLine($"Input integer {size} elements of array: ");
+    for (int i = 0; i<size; i++)
+    {
+        Console.Write($"{i} - ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int FindForArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Input the size of the array");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = InputArray(size);
+int count = FindForArray(arr);
+Console.WriteLine($"Input of number > zero is {count}");
