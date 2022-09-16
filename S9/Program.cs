2c9d2e65aafd3eeedfd17b8e2832fd40b8c537b8
SolitE <solitte@yandex.ru>
2022-09-17 04:28:17 +0500

    DZ_S9

--- S9/Program.cs
+++ S9/Program.cs
@@ -39,3 +39,20 @@ Console.WriteLine(SumNumers(m, n));
 */
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Int64 Akkerman(Int64 m, Int64 n)
+{
+    if(m > 0 && n > 0) return Akkerman(m-1,Akkerman(m,n-1));
+    if(m > 0 && n == 0) return Akkerman(m-1, 1);
+    if(m == 0) return n+1;
+    return 0; // здесь любое число, так как все значения m,n обработаны через return, программа сюда незайдет.
+   
+}
+
+
+
+Console.WriteLine("Input two integer numbers");
+Int64 m = Convert.ToInt64(Console.ReadLine());
+Int64 n = Convert.ToInt64(Console.ReadLine());
+Int64 akkerman = Akkerman(m, n);
+Console.WriteLine($"Value function of Akkerman is {akkerman}");
